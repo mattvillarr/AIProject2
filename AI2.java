8dc925f47383f45518a2814931a1e9e459786b93
CoolAnimalName <mattvillarr@gmail.com>
2017-03-05 21:01:18 -0600

    added Node class methods and .con parsing to AI2

--- AI2.java
+++ AI2.java
@@ -16,23 +16,34 @@ public class AI2 {
 
       Scanner vScan = null;
       Scanner cScan = null;
-      try { //var file parsing
+      /*try { //var file parsing
 
       } //end try
       catch(FileNotFoundException e) {
         e.printStackTrace();
-      } //end catch
+      } //end catch */
+
+      List<char[]> constr = new ArrayList<char[]>();
 
       try { //con file parsing
-
+        cScan = new Scanner(conF);
+        while(cScan.hasNextLine()) {
+          char[] in = cScan.nextLine().toCharArray();
+          char[] arr = {in[0], in[2], in[4]};
+          constr.add(arr);
+        } //end while
+        //for(int i = 0; i < constr.size(); i++) {
+          //char[] got = constr.get(i);
+          //System.out.println(got[0] + " " + got[1] + " " + got[2]);
+        //} //end for
       } //end try
       catch(FileNotFoundException f) {
         f.printStackTrace();
       } //end catch
 
-
       if(args[2].equals("fc"))
         fc = true;
 
+    List<Node> space = new LinkedList<Node>();
   } //end main
 } //end class AI2
--- Node.java
+++ Node.java
@@ -3,11 +3,47 @@ CS 4365.001
 Colleen Cousins
 Matthew Villarreal
 ******************************************************/
+import java.util.*;
 public class Node {
 
+  Map<String, List<Integer>> vars = new HashMap<String, List<Integer>>();
+  List<Integer> assignment = new ArrayList<Integer>();
 
+  public Node(Map<String, List<Integer>> original) { //constructor
+    for(Map.Entry<String, List<Integer>> orig : original.entrySet()) //copy map
+      vars.put(orig.getKey(), new ArrayList<Integer>(orig.getValue()));
 
+  } //end Node(2)
 
+  public Node(Node other) {
+    for(Map.Entry<String, List<Integer>> orig : other.vars.entrySet()) //copy map
+      this.vars.put(orig.getKey(), new ArrayList<Integer>(orig.getValue()));
+
+    this.assignment = new ArrayList<Integer>(other.assignment);
+  } //end Node(1)
+
+  private int varHuer() {
+    return 0;
+  } //end varHuer
+
+  private int valHuer() {
+    return 0;
+  } //end valHuer
+
+  public void getAssign() {
+    int var = varHuer();
+    int val = valHuer();
+    //check to make sure var is already not in the list
+    //either replace it in the list with new assignment or add to end of list
+  } //end getAssign
+
+  public boolean constrCheck() {
+    return false;
+  } //end constrCheck
+
+  public void printAssignment() {
+
+  } //end printAssignment
 
 
 
