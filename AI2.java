12702061a8b8a71685a34d63c75f7e064fa27fda
CoolAnimalName <mattvillarr@gmail.com>
2017-03-05 19:12:01 -0600

    started AI2.java

--- AI2.java
+++ AI2.java
@@ -9,5 +9,30 @@ import java.io.*;
 public class AI2 {
   public static void main(String[] args) {
 
+      Boolean fc = false;
+
+      File varF = new File(args[0]);
+      File conF = new File(args[1]);
+
+      Scanner vScan = null;
+      Scanner cScan = null;
+      try { //var file parsing
+
+      } //end try
+      catch(FileNotFoundException e) {
+        e.printStackTrace();
+      } //end catch
+
+      try { //con file parsing
+
+      } //end try
+      catch(FileNotFoundException f) {
+        f.printStackTrace();
+      } //end catch
+
+
+      if(args[2].equals("fc"))
+        fc = true;
+
   } //end main
 } //end class AI2
