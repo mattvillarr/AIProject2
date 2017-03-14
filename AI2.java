87cb10e306c741f6ac3776744933d1618b19abd2
CoolAnimalName <mattvillarr@gmail.com>
2017-03-14 18:20:39 -0500

    help

--- AI2.java
+++ AI2.java
@@ -9,41 +9,65 @@ import java.io.*;
 public class AI2 {
   public static void main(String[] args) {
 
-      Boolean fc = false;
+    Boolean fc = false;
 
-      File varF = new File(args[0]);
-      File conF = new File(args[1]);
+    File varF = new File(args[0]);
+    File conF = new File(args[1]);
 
-      Scanner vScan = null;
-      Scanner cScan = null;
-      /*try { //var file parsing
+    Scanner vScan = null;
+    Scanner cScan = null;
+    Map<String, Set<Integer>> vList = new HashMap<String, Set<Integer>>();
 
-      } //end try
-      catch(FileNotFoundException e) {
-        e.printStackTrace();
-      } //end catch */
+    List<char[]> constr = new ArrayList<>();
 
-      List<char[]> constr = new ArrayList<char[]>();
+    try {
+      vScan = new Scanner(varF);
 
-      try { //con file parsing
-        cScan = new Scanner(conF);
-        while(cScan.hasNextLine()) {
-          char[] in = cScan.nextLine().toCharArray();
-          char[] arr = {in[0], in[2], in[4]};
-          constr.add(arr);
-        } //end while
+      while(vScan.hasNext()) {
+        String key = vScan.next();
+        Set<Integer> tempList = new HashSet<>();
+
+        while(vScan.hasNextInt())
+          tempList.add(vScan.nextInt());
+
+          vList.put(key.substring(0, 1), tempList);
+      } //end while
+
+        /*for(Map.Entry entry : vList.entrySet()) {
+          System.out.println(entry.getKey() + ", " + entry.getValue());
+        } //end for */
+    } //end try
+    catch(FileNotFoundException g) {
+      g.printStackTrace();
+    } //end catch
+
+    try { //con file parsing
+      cScan = new Scanner(conF);
+      while(cScan.hasNextLine()) {
+        char in[] = cScan.nextLine().toCharArray();
+        char arr[] = {in[0], in[2], in[4]};
+        constr.add(arr);
+      } //end while
         //for(int i = 0; i < constr.size(); i++) {
           //char[] got = constr.get(i);
           //System.out.println(got[0] + " " + got[1] + " " + got[2]);
         //} //end for
-      } //end try
-      catch(FileNotFoundException f) {
-        f.printStackTrace();
-      } //end catch
+    } //end try
+    catch(FileNotFoundException f) {
+      f.printStackTrace();
+    } //end catch
 
-      if(args[2].equals("fc"))
-        fc = true;
+    if(args[2].equals("fc"))
+      fc = true;
 
-    List<Node> space = new LinkedList<Node>();
+    LinkedList<Node> space = new LinkedList<Node>();
+    Node head = new Node(vList);
+    space.addFirst(head);
+
+    Node next = null;
+
+    for(int i=0; i < 30; i++) {
+      next = new Node(head);
+    } //end for
   } //end main
 } //end class AI2
--- Node.java
+++ Node.java
@@ -6,21 +6,33 @@ Matthew Villarreal
 import java.util.*;
 public class Node {
 
-  Map<String, List<Integer>> vars = new HashMap<String, List<Integer>>();
-  List<Integer> assignment = new ArrayList<Integer>();
+  public Map<String, Set<Integer>> vars = new HashMap<String, Set<Integer>>();
+  public List<Integer> assignment = new ArrayList<Integer>();
+  int lcv = -1;
+  int maxvals = -1;
 
-  public Node(Map<String, List<Integer>> original) { //constructor
-    for(Map.Entry<String, List<Integer>> orig : original.entrySet()) //copy map
-      vars.put(orig.getKey(), new ArrayList<Integer>(orig.getValue()));
+  public Node(Map<String, Set<Integer>> original) { //constructor
+    for(Map.Entry<String, Set<Integer>> orig : original.entrySet()) //copy map
+      vars.put(orig.getKey(), new HashSet<Integer>(orig.getValue()));
 
   } //end Node(2)
 
   public Node(Node other) {
-    for(Map.Entry<String, List<Integer>> orig : other.vars.entrySet()) //copy map
-      this.vars.put(orig.getKey(), new ArrayList<Integer>(orig.getValue()));
+    for(Map.Entry<String, Set<Integer>> orig : other.vars.entrySet()) //copy map
+      this.vars.put(orig.getKey(), new HashSet<Integer>(orig.getValue()));
 
     this.assignment = new ArrayList<Integer>(other.assignment);
   } //end Node(1)
+
+  public Node(Node parent, String key, int val) {
+    for(Map.Entry<String, Set<Integer>> entry : parent.vars.entrySet()) {
+      String check = entry.getKey();
+      if(key.equals(check))
+        this.vars.put(entry.getKey(), new HashSet<Integer>(val));
+      else
+        this.vars.put(entry.getKey(), new HashSet<Integer>(entry.getValue()));
+    } //end for
+  } //end Node(3)
 
   private int varHuer() {
     return 0;
@@ -42,7 +54,10 @@ public class Node {
   } //end constrCheck
 
   public void printAssignment() {
-
+    for (int i = 0; i < assignment.size(); i++) {
+      System.out.print((char)65 + i);
+      System.out.print(" : " + assignment.get(i) + " \n");
+    } //end for
   } //end printAssignment
 
 
